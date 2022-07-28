db68c94bae2161ce0cb066d5a5e9b40d17de9126
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2022-07-28 11:46:45 +0530

    xcvxc

--- facebook/src/main/java/com/facebook/dao/FacebookDAO.java
+++ facebook/src/main/java/com/facebook/dao/FacebookDAO.java
@@ -5,48 +5,97 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import com.facebook.entity.FacebookUser;
 
 public class FacebookDAO implements FacebookDAOInterface {
 
 	public int createProfileDAO(FacebookUser fb) {
+		
+		
 		int i=0;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/revatureadvance","root","rajesh");
-			PreparedStatement ps=con.prepareStatement("insert into facebookuser values(?,?,?,?)");
-			ps.setString(1, fb.getName());
-			ps.setString(2, fb.getPassword());
-			ps.setString(3, fb.getEmail());
-			ps.setString(4, fb.getAddress());
-			
-			i=ps.executeUpdate();
-		}
-		catch(ClassNotFoundException|SQLException e) {
-			e.printStackTrace();
-		}
+		/*
+		 * try { Class.forName("com.mysql.jdbc.Driver"); Connection
+		 * con=DriverManager.getConnection("jdbc:mysql://localhost:3306/revatureadvance"
+		 * ,"root","rajesh"); PreparedStatement
+		 * ps=con.prepareStatement("insert into facebookuser values(?,?,?,?)");
+		 * ps.setString(1, fb.getName()); ps.setString(2, fb.getPassword());
+		 * ps.setString(3, fb.getEmail()); ps.setString(4, fb.getAddress());
+		 * 
+		 * i=ps.executeUpdate(); } catch(ClassNotFoundException|SQLException e) {
+		 * e.printStackTrace(); }
+		 */
+		
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		/*
+		 * in above configure method by default will load hibernate.cfg.xml file from src/main/java and provide all information to buildsessionfactory() method
+		 * buildsessionfactory() method will create database connection , create table, do the mapping between table and entity
+		 * by default SessionFactory class is singelton
+		 * sessionfactory level cache is known as second level cache or L2 cache
+		 */
+		Session s=sf.openSession();
+		/*
+		 * to do any task we have to create individual session using sessionfactory object
+		 * session level cache is known as first level cache or L1 cache
+		 */
+		EntityTransaction et=s.getTransaction();
+		
+		/*
+		 * entitytransaction is required for DML operation like insert, delete, update or save(), delete() and update() method
+		 * we must start transaction before running any DML operation and then we must commit our transaction.
+		 */
+		
+		et.begin();
+		
+			s.save(fb);
+		
+		et.commit();
+		
+		
+		i=1;
+		
+		
+		
 		return i;
 	}
 
 	@Override
 	public boolean loginProfileDAO(FacebookUser fb) {
 		boolean i=false;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/revatureadvance","root","rajesh");
-			PreparedStatement ps=con.prepareStatement("select * from facebookuser where email=? and password=?");
-			ps.setString(1, fb.getEmail());
-			ps.setString(2, fb.getPassword());
+		/*
+		 * try { Class.forName("com.mysql.jdbc.Driver"); Connection
+		 * con=DriverManager.getConnection("jdbc:mysql://localhost:3306/revatureadvance"
+		 * ,"root","rajesh"); PreparedStatement ps=con.
+		 * prepareStatement("select * from facebookuser where email=? and password=?");
+		 * ps.setString(1, fb.getEmail()); ps.setString(2, fb.getPassword());
+		 * 
+		 * ResultSet res=ps.executeQuery(); if(res.next()) { i=true; } }
+		 * catch(ClassNotFoundException|SQLException e) { e.printStackTrace(); }
+		 */
+		
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session s=sf.openSession();
+		
+		/*
+		 * if our query is undecided or inbuild methods are not available to handle such type of query then we should HQL and should handle query as below
+		 */
+			Query q=s.createQuery("from com.facebook.entity.FacebookUser f where f.email=:em and f.password=:pw");
+			q.setParameter("em", fb.getEmail());
+			q.setParameter("pw", fb.getPassword());
 			
-			ResultSet res=ps.executeQuery();
-			if(res.next()) {
+			List<FacebookUser> ff=q.getResultList();
+			if(ff.size()>0) {
 				i=true;
 			}
-		}
-		catch(ClassNotFoundException|SQLException e) {
-			e.printStackTrace();
-		}
+		
 		return i;
 	}
 
--- facebook/src/main/java/com/facebook/entity/FacebookUser.java
+++ facebook/src/main/java/com/facebook/entity/FacebookUser.java
@@ -1,8 +1,26 @@
 package com.facebook.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity   //this annotation help to map entity on table
+@Table(name = "socialmedia") //this one help to change table name
 public class FacebookUser {
+	
+	@Column(name="username", length = 34) //this annotation help to change structure of column in table like name size etc.
 	private String name;
 	private String password;
+	
+	@Id   //this annotation help to create primary key
+	@GeneratedValue(strategy = GenerationType.AUTO) //this line help to generate primary key automatically
+	private long userId;
+	
+	
+	
 	private String email;
 	private String address;
 	public String getName() {
